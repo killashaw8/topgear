a9edda7eff8d6341341285d9134c12ac28681843
killashaw8 <iamhalimov@gmail.com>
2025-07-08 18:29:14 +0900

    feat: Develop sessions logic

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,18 @@
 import express from "express";
 import path from "path";
 import morgan from "morgan";
+import session from "express-session";
 import { MORGAN_FORMAT } from "./libs/config";
 import router from "./router";
 import routerAdmin from "./router-admin";
+import ConnectMongoDBSession from "connect-mongodb-session";
+import { T } from "./libs/types/common";
+
+const MongoDBStore = ConnectMongoDBSession(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions"
+})
 
 // Entrance
 const app = express();
@@ -13,9 +22,34 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
+// Sessions
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 5         //5 hours
+    }, 
+    store: store,                        //DB collection
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+app.use(
+  function(req, res, next) {
+    const sessionInstance = req.session as T;
+    res.locals.member = sessionInstance.member;
+    next();
+  }
+)
+
+// Views
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 // Routers
-app.use("/admin", routerAdmin);
-app.use("/", router);
+app.use("/admin", routerAdmin);   //SSR => EJS
+app.use("/", router);             //SPA => React
 
 
 
